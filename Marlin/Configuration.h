0405dd0cbe5007c864dd93c8ec6a49e767e621e0
XcLxAd <artemovann@gmail.com>
2020-12-03 17:22:30 +0300

    Disabled commands to execute at the end of G29 probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1373,7 +1373,7 @@
  * Commands to execute at the end of G29 probing.
  * Useful to retract or move the Z probe out of the way.
  */
-#define Z_PROBE_END_SCRIPT "G28\nM500"
+//#define Z_PROBE_END_SCRIPT "G28\nM500"
 
 // @section homing
 
